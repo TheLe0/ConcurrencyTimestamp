7186646adc993e47691440d20920731aa0eacd37
TheLe0 <leob.tosin@hotmail.com>
2020-11-15 19:15:55 -0300

    First commit

---
+++ ConcurrencyTimestamp/ConcurrencyTimestamp/Simulator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrencyTimestamp
+{
+    class Simulator
+    {
+        readonly List<string> transactionList;
+        Dictionary<string, int> x;
+        Dictionary<string, int> y;
+        Dictionary<string, int> z;
+
+        public Simulator(List<string> list)
+        {
+            transactionList = list;
+        }
+
+        public void StartSimulator()
+        {
+            Console.WriteLine("-- História --");
+            int _i = 1;
+
+            foreach(string transaction in transactionList)
+            {
+                Console.WriteLine("H"+_i+" = "+ transaction);
+                _i++;
+            }
+
+            x = new Dictionary<string, int>()
+            {
+                { "r", 0 }, { "w", 0 }
+            };
+
+            y = new Dictionary<string, int>()
+            {
+                { "r", 0 }, { "w", 0 }
+            };
+
+            z = new Dictionary<string, int>()
+            {
+                { "r", 0 }, { "w", 0 }
+            };
+
+            this.Schedule();
+        }
+
+        private void Schedule()
+        {
+
+        }
+    }
+}
---
+++ ConcurrencyTimestamp/ConcurrencyTimestamp/UserInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcurrencyTimestamp
+{
+    class UserInterface
+    {
+        List<string> transactionList;
+        Simulator simulator;
+
+        public void ShowUserInput()
+        {
+            int _i = 1;
+            string _transaction;
+            transactionList = new List<string>();
+            Console.WriteLine("Digite as transações, quando não quiser mais, deixe a linha em branco");
+
+            Console.WriteLine("Digite a transação "+_i+": ");
+            _transaction = Console.ReadLine();
+
+            while (_transaction != "")
+            {
+                transactionList.Add(_transaction);
+                _i++;
+                Console.WriteLine("Digite a transação " + _i + ": ");
+                _transaction = Console.ReadLine();
+            }
+
+            this.InitSimulator();
+        }
+
+        private void InitSimulator()
+        {
+            simulator = new Simulator(transactionList);
+            simulator.StartSimulator();
+        }
+    }
+}
