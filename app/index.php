be07bf8bff6e86ef1f3c324ab7f36286f8be81ee
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2020-11-18 20:21:51 -0300

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+        <form action="./timestamp.php" method="POST">
+            <div id='div' align='center'>
+            	<p><strong>Ex.: w-x r-y r-x w-z</strong></p>
+                <table>
+                	<tr>
+                		<td>Transaction 1: <input type="text" id="t[]" name="t[]"></td>
+                	</tr>
+                	<tr>
+                		<td>Transaction 2: <input type="text" id="t[]" name="t[]"></td>
+                	</tr>
+                	<tr>
+                		<td>Transaction 3: <input type="text" id="t[]" name="t[]"></td>
+                	</tr>
+                	<tr>
+                		<td>Transaction 4: <input type="text" id="t[]" name="t[]"></td>
+                	</tr>
+                	<tr>
+                		<td>Transaction 5: <input type="text" id="t[]" name="t[]"></td>
+                	</tr>
+                </table>
+                <input type="submit">
+            </div>
+        </form>
+    </body>
+</html>
